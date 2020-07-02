641194b42d9d09b36782af3fde5c11ce87d5828d
PrasadTelasula <Prasad.Telasula@yahoo.com>
2020-07-03 02:27:02 +0530

    Startup & Cleanup scripts Modified

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
+REGION="ap-south-1"
+S3_BUCKET_NAME="terraform-formac-state-file"
+DYNAMODB_TABLE_NAME="terraform-formac-state-lock"
 
+# Remove dynamodb table
 aws dynamodb delete-table \
-    --table-name terraform-formac-state-lock \
-    --region ap-south-1
+    --table-name $DYNAMODB_TABLE_NAME \
+    --region $REGION
 
+if [ $? -eq 0 ]; then
+   echo "+ DynamoDB Table: $DYNAMODB_TABLE_NAME was deleted."
+   echo
+fi
+
+# Empty S3 Bucket
 aws s3 rm \
-    --recursive s3://terraform-formac-state-file \
-    --region ap-south-1
+    --recursive s3://$S3_BUCKET_NAME \
+    --region $REGION
 
+# Remove S3 Bucket
 aws s3 rb \
-    s3://terraform-formac-state-file \
-    --region ap-south-1
+    s3://$S3_BUCKET_NAME \
+    --region $REGION
+
+if [ $? -eq 0 ]; then
+   echo "+ Bucket: $S3_BUCKET_NAME was deleted."
+   echo
+fi
+
+# Remove Available volumes
+volumeids=`aws ec2 describe-volumes \
+	   --filter "Name=status,Values=available" \
+	   --query 'Volumes[*].{VolumeID:VolumeId}' \
+	   --region $REGION \
+	   --output text`
+
+for i in $volumeids
+do
+   aws ec2 delete-volume --volume-id $i --region ap-south-1
+   echo "+ VolumeID: $i is Deleted."
+done
+
+# Delete local keys folder
+if [ -d "keys" ]; then
+   rm -rf keys
+fi
+
+# Delete .terraform  folder
+if [ -d ".terraform" ]; then
+   rm -rf .terraform
+fi
--- startup.sh
+++ startup.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
+REGION="ap-south-1"
+S3_BUCKET_NAME="terraform-formac-state-file"
+DYNAMODB_TABLE_NAME="terraform-formac-state-lock"
 
-
+# Create Keys folder Locally.
 if [ -d "keys" ]; then
-  echo "keys Directory already exists" ;
+  echo "+ keys Directory already exists" ;
+  echo 
 else
   mkdir -p keys;
-  echo "keys directory is created"
+  echo "+ keys directory is created"
+  echo
 fi
 
 
+# Generate SSH Keyfiles for all servers.
 keylist=( "acsLaunchKey" "centosLaunchKey" "ubuntuLaunchKey" "windowsLaunchKey" )
 
 
@@ -16,22 +22,37 @@ for i in "${keylist[@]}"
 do
   if [ ! -f keys/$i ]; then
     ssh-keygen -t rsa -m PEM -f keys/$i -q -N ""
-    echo "$i is Created."
+    echo "+ $i is Created."
+    echo
   else
-    echo "$i is already exists."
+    echo "+ $i is already exists."
+    echo
   fi 
 done
 
-
+# Create S3 Bucket to store the state file.
 aws s3 mb \
-    s3://terraform-formac-state-file \
-    --region ap-south-1 
+    s3://$S3_BUCKET_NAME \
+    --region $REGION
 
+if [ $? -eq 0 ]; then
+   echo "+ Bucket: $S3_BUCKET_NAME Created in $REGION region."
+   echo
+fi
+
+# Create DynamoDB Table to store the terraform state lock.
 aws dynamodb create-table \
-    --table-name terraform-formac-state-lock \
+    --table-name $DYNAMODB_TABLE_NAME \
     --attribute-definitions AttributeName=LockID,AttributeType=S \
     --key-schema AttributeName=LockID,KeyType=HASH \
-    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --region $REGION
+
+if [ $? -eq 0 ]; then
+   echo "+ DynamoDB Table: $DYNAMODB_TABLE_NAME Created in $REGION region."
+   echo
+fi
+
 
 
 
